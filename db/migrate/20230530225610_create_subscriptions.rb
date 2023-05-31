ffcddbb9694a03fd2dbbe18ed3e2973b2425cf15
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-05-31 14:23:25 -0600

    Update db

--- db/migrate/20230530225610_create_subscriptions.rb
+++ db/migrate/20230530225610_create_subscriptions.rb
@@ -1,8 +1,10 @@
 class CreateSubscriptions < ActiveRecord::Migration[5.2]
   def change
     create_table :subscriptions do |t|
-      t.string :status
+      t.title :string
       t.float :price
+      t.string :status
+      t.frequency :string
       t.references :customer, foreign_key: true
 
       t.timestamps
--- db/migrate/20230530230029_create_teas.rb
+++ db/migrate/20230530230029_create_teas.rb
@@ -1,8 +1,10 @@
 class CreateTeas < ActiveRecord::Migration[5.2]
   def change
     create_table :teas do |t|
-      t.string :name
-      t.text :description
+      t.string :title
+      t.string :description
+      t.integer :temperature
+      t.string :brew_time
       t.references :subscription, foreign_key: true
 
       t.timestamps
