505bf22d6a87ef088d17e2d0a2525ac9ad6e8c79
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-06-01 10:16:19 -0600

    Update spec filel name

--- spec/requests/subscriptions_request_spec.rb
+++ spec/requests/subscriptions_request_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe "Subscriptions", type: :request do
     @customer1 = create(:customer)
     @tea1 = create(:tea)
     @tea2 = create(:tea)
-    @subscription1 = create(:subscription, customer: @customer1, tea: @tea1)
-    @subscription2 = create(:subscription, customer: @customer1, tea: @tea2)
+    @subscription1 = create(:subscription, status: "active", frequency: "weekly", customer: @customer1, tea: @tea1)
+    @subscription2 = create(:subscription, status: "inactive", frequency: "monthly", customer: @customer1, tea: @tea2)
   end
 
   describe "/api/v1/customers/:customer_id/subscriptions" do
     it "returns all subscriptions for a customer" do
       get "/api/v1/customers/#{@customer1.id}/subscriptions"
-
+binding.pry
       expect(response).to be_successful
       expect(response.status).to eq(200)
     end
