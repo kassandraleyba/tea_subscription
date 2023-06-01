8ab8badac692679f0520279b3a79c8958e9d4a85
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-06-01 08:04:34 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :customers do
+        resources :subscriptions, only: [:index, :create, :destroy]
+        # "/api/v1/customers/:customer_id/subscriptions"
+      end
+    end
+  end
 end
