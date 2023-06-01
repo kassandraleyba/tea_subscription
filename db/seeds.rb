2de6a5fc7254259b984045e5178954a8ee6812c0
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-06-01 10:11:33 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer1 = Customer.create(first_name: "John", last_name: "Doe", email: "john.doe@example.com", address: "123 Main St")
+customer2 = Customer.create(first_name: "Jane", last_name: "Smith", email: "jane.smith@example.com", address: "456 Elm St")
+
+tea1 = Tea.create(title: "Green Tea", description: "Refreshing green tea", temperature: 80, brew_time: 3)
+tea2 = Tea.create(title: "Black Tea", description: "Bold black tea", temperature: 95, brew_time: 5)
+
+subscription1 = Subscription.create(title: "Monthly Green Tea", price: 10.99, status: 0, frequency: 2, customer: customer1, tea: tea1)
+subscription2 = Subscription.create(title: "Weekly Black Tea", price: 8.99, status: 0, frequency: 0, customer: customer2, tea: tea2)
