887a987b8ae1b40525b29f0c8858a4df280336ea
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-06-01 19:43:10 -0600

    Update validations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Subscription, type: :model do
     it { should belong_to :customer }
     it { should belong_to :tea }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+    it { should validate_presence_of :price }
+  end
 end
